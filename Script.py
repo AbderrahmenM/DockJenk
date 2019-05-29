491ec33c3f2008202a81921563912304d4a77705
AbderrahmenM <abderrahmenmrabet1@gmail.com>
2019-05-29 11:48:06 +0200

    Repo creation

---
+++ Script.py
@@ -0,0 +1,41 @@
+"""
+HelloWorld.py
+====================================
+Document créé à 15h25.
+Doc généré par Sphinx2.0.
+"""
+import platform
+from mod import Mod
+
+def confession(your_name):
+    """
+    Return the most important secret of a person.
+    
+    Parameters
+    ----------
+    your_name
+        A string indicating the name of the person.
+    """
+    print("The wise {} loves Python.".format(your_name))
+
+def version():
+    """
+    Return the Python version used by this session.
+    """
+    print(platform.python_version())
+
+def modulo(FirstInt, SecondInt):
+    """
+    Return the result of the divison of a by b.
+    
+    Parameters
+    ----------
+    your_name
+        FirstInt Int indicating the numerator.
+        SecondInt Int indicating the denominatorur.
+    """    
+    print(Mod(FirstInt, SecondInt)) 
+
+version()
+confession("AB")
+modulo(10, 4)
